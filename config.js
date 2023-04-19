35cc650b567f47ff7c6894c96036ff769472786a
sungsu97 <tjdtn7586@gmail.com>
2023-04-19 12:57:23 +0900

    gg

---
+++ config.js
@@ -0,0 +1,4 @@
+export const contractAddress = "0xFB6d689D83A44257D6c13c2354F96A57c7085455";
+export const INFURA_URL = "https://goerli.infura.io/v3/704cd728f9584cc5b6d64137af3da13a";
+export const PINATA_KEY = "6d40784a074c56c87bbb";
+export const PINATA_SECRET = "caca2ebf9870f288975cad72bbe81e5f820a25e6242bac74a1b3ead098115a27";
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+    <nav className='border-b p-6'>
+      <p className='text-4xl font-bold'>Metaverse NFT Store</p>
+      <div className='flex mt-4'></div>
+      <Link href='/' className='mr-4 text-pink-500'>
+        Home
+      </Link>
+      <Link href='/create-nft' className='mr-6 text-pink-500'>
+         Sell NFT
+      </Link>
+      <Link href='/my-nfts' className='mr-4 text-pink-500'>
+         My NFT
+      </Link>
+      <Link href='/creator-dashboard' className='mr-4 text-pink-500'>
+        Dashboard
+      </Link>
+
+    </nav>
+   <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
